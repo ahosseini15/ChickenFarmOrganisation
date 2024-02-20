b92f38ea35804febd8a470faf8e3c809badcfe3e
hali <alhosseini@united-internet.de>
2024-02-20 15:51:38 +0100

    start

---
+++ ChickenfarmOrganizr/src/main/java/com/ChickenFarm/ChickenfarmOrganizr/ChickenfarmOrganizrApplication.java
@@ -0,0 +1,13 @@
+package com.ChickenFarm.ChickenfarmOrganizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenfarmOrganizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenfarmOrganizrApplication.class, args);
+	}
+
+}
---
+++ ChickenfarmOrganizr/src/test/java/com/ChickenFarm/ChickenfarmOrganizr/ChickenfarmOrganizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChickenFarm.ChickenfarmOrganizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenfarmOrganizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
